9ada8c64d6ccc6872475dce1ebff264e21baf7f8
jcruz781 <60797254+jcruz781@users.noreply.github.com>
2020-05-21 21:40:22 -0700

    Add files via upload

---
+++ Lab 20_1/src/Lab/FirstProgram.java
@@ -0,0 +1,13 @@
+package Lab;
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.print("Hello, ");
+        System.out.println("World!!!");
+        System.out.print("My name is Jorge Cruz.");
+        System.out.println(" ");
+        System.out.println("Welcome to CSE20 at UC Merced.");
+	}
+
+}
